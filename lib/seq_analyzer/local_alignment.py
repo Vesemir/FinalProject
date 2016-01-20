75368fae55b17472ca01a031ecf877aa617cb860
Vesemir <somemail@anywhere.com>
2016-01-20 21:59:26 +0300

    Disabled scoring matrix since we are matching only EXACT same values for now

--- lib/seq_analyzer/local_alignment.py
+++ lib/seq_analyzer/local_alignment.py
@@ -2,7 +2,14 @@ from itertools import chain
 from collections import deque
 
 
-ALPH = [each for each in range(1, 100)]
+SCORE_MATCH = 10
+SCORE_EMPTY = -1
+SCORE_DIFFERENT = 0
+
+def scoring_matrix(rec_1, rec_2):
+    return SCORE_MATCH if rec_1 == rec_2 else SCORE_EMPTY\
+           if rec_1 == '-' or rec_2 == '-'\
+           else SCORE_DIFFERENT
 
 
 def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
@@ -14,28 +21,28 @@ def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
             for symrow in chain(alphabet, '-')}
 
 
-def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
+def compute_alignment_matrix(seq_x, seq_y, global_flag):
     lenfirst, lensecond = len(seq_x), len(seq_y)
     matr = [[0 for _ in range(lensecond + 1)] for __ in range(lenfirst + 1)]
     for idx in range(1, lenfirst + 1):
-        temp = matr[idx-1][0] + scoring_matrix[seq_x[idx-1]]['-']
+        temp = matr[idx-1][0] + scoring_matrix(seq_x[idx-1], '-')
         matr[idx][0] = temp if global_flag else 0
     for jdx in range(1, lensecond + 1):
-        temp = matr[0][jdx-1] + scoring_matrix['-'][seq_y[jdx-1]]
+        temp = matr[0][jdx-1] + scoring_matrix('-', seq_y[jdx-1])
         matr[0][jdx] = temp if global_flag else 0
     for idx in range(1, lenfirst + 1):
         for jdx in range(1, lensecond + 1):
             res = max((matr[idx-1][jdx-1] +
-                                  scoring_matrix[seq_x[idx-1]][seq_y[jdx-1]]),
+                                  scoring_matrix(seq_x[idx-1], seq_y[jdx-1])),
                                  (matr[idx-1][jdx] +
-                                  scoring_matrix[seq_x[idx-1]]['-']),
+                                  scoring_matrix(seq_x[idx-1], '-')),
                                  (matr[idx][jdx-1] +
-                                  scoring_matrix['-'][seq_y[jdx-1]]))
+                                  scoring_matrix('-', seq_y[jdx-1])))
             matr[idx][jdx] = res if global_flag else res if res >0 else 0
     return matr
 
 
-def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
+def compute_local_alignment(seq_x, seq_y, alignment_matrix):
     maxpos = max(((hoel, (idx, jdx)) for idx, line in enumerate(alignment_matrix) for (jdx, hoel) in enumerate(line) ), key=lambda x: x[0])
     idx, jdx = maxpos[1]
     xslash, yslash = deque(), deque()
@@ -45,13 +52,13 @@ def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
             break
         total = max(total, alignment_matrix[idx][jdx])
         if alignment_matrix[idx][jdx] == alignment_matrix[idx-1][jdx-1] +\
-           scoring_matrix[seq_x[idx-1]][seq_y[jdx-1]]:
+           scoring_matrix(seq_x[idx-1], seq_y[jdx-1]):
             xslash.appendleft(seq_x[idx-1])
             yslash.appendleft(seq_y[jdx-1])
             idx -= 1
             jdx -= 1
         elif alignment_matrix[idx][jdx] == alignment_matrix[idx-1][jdx] +\
-             scoring_matrix[seq_x[idx-1]]['-']:
+             scoring_matrix(seq_x[idx-1], '-'):
             xslash.appendleft(seq_x[idx-1])
             yslash.appendleft('-')
             idx -= 1
@@ -74,8 +81,7 @@ def compute_local_alignment(seq_x, seq_y, scoring_matrix, alignment_matrix):
     return total, xslash, yslash
 
 
-
-def compute_alignment_helper(seq_x, seq_y, alphabet=ALPH, diag=5, off_diag=1, dash=-2):
-    scoring_mat = build_scoring_matrix(alphabet, diag, off_diag, dash)
-    align_mat = compute_alignment_matrix(seq_x, seq_y, scoring_mat, 0)
-    return compute_local_alignment(seq_x, seq_y, scoring_mat, align_mat)
+def compute_alignment_helper(seq_x, seq_y, diag=10, off_diag=0, dash=-1):
+    #scoring_mat = build_scoring_matrix(alphabet, diag, off_diag, dash)
+    align_mat = compute_alignment_matrix(seq_x, seq_y, 0)
+    return compute_local_alignment(seq_x, seq_y, align_mat)
