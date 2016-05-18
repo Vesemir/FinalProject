34205b026f4a2db342a53f0d627f0ff8513e7d62
Vesemir <somemail@anywhere.com>
2016-05-18 11:47:52 +0300

    Added fast scorign matrix builder by cython

--- lib/seq_analyzer/scoring/compare_samples.pyx
+++ lib/seq_analyzer/scoring/compare_samples.pyx
@@ -9,8 +9,36 @@ ctypedef np.int32_t DINT32_t
 ctypedef np.float64_t DFLOAT64_t
 
 cdef int SCORE_MATCH = 10
+cdef int SCORE_MATCH_IMPORTANT = 13
 cdef int SCORE_EMPTY = -6
 cdef int SCORE_DIFFERENT = -2
+
+
+def build_scoring_matrix(int size,
+                         np.ndarray[DINT32_t, ndim=1] MUTED_CALLS,
+                         np.ndarray[DINT32_t, ndim=1] IMPORTANT_CALLS):
+    cdef int diag_score = SCORE_MATCH
+    cdef int off_diag_score = SCORE_DIFFERENT
+    cdef int dash_score = SCORE_EMPTY
+    cdef int diag_important_score = SCORE_MATCH_IMPORTANT
+    cdef int symrow, symcol
+    cdef np.ndarray[DINT32_t, ndim=2] matr = np.zeros(
+        (size + 1, size + 1), dtype=DINT32)
+    for symrow in range(size + 1):
+        for symcol in range(size + 1):
+            if not symcol or not symrow:
+                matr[symrow,symcol] = dash_score
+            elif symcol == symrow:
+                if symcol in MUTED_CALLS:
+                    matr[symrow,symcol] = 0# or 0
+                elif symcol in IMPORTANT_CALLS:
+                    matr[symrow,symcol] = diag_important_score
+                else:
+                    matr[symrow,symcol] = diag_score
+            elif symcol != symrow:
+                matr[symrow,symcol] = off_diag_score
+    return matr
+
 
 def compute_alignment_matrix(np.ndarray[DINT32_t, ndim=1] seq_x,
                              np.ndarray[DINT32_t, ndim=1] seq_y,
